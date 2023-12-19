6918e7db2522f69879091e39fb258fde3d54b435
HAIXIN ZHANG <haixin.zhang.777@gmail.com>
2023-12-19 17:23:52 +0800

    web-17

---
+++ frontend_example/src/js/database/database.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { getPath } from "@/js/database/utils.js";
+
+const fetchData = async (endpoint) => {
+  try {
+    const data = await axios.get(endpoint);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getDatabase = async () => {
+  const [cards, profile, highline, message, request] = await Promise.all([
+    (async () => (await fetchData(getPath("cards"))).data)(),
+    (async () => (await fetchData(getPath("profile"))).data)(),
+    (async () => (await fetchData(getPath("highline"))).data)(),
+    (async () => (await fetchData(getPath("message"))).data)(),
+    (async () => (await fetchData(getPath("request"))).data)(),
+  ]);
+
+  return { cards, profile, highline, message, request };
+};
